0acc66b669ac659c31bb174c3efb7eccbedbc7e2
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-21 15:00:59 +0200

    add jenkins

---
+++ vendor/github.com/bborbe/backup_rsync/model/model.go
@@ -0,0 +1,63 @@
+package model
+
+import (
+	"fmt"
+	"github.com/bborbe/io/util"
+	"io/ioutil"
+	"strings"
+)
+
+type BackupSourceBaseDirectory string
+
+type BackupSourceDirectory string
+
+func (b BackupSourceDirectory) String() string {
+	return appendSlash(string(b))
+}
+
+type RemoteHost string
+
+type RemotePort int
+
+type RemoteUser string
+
+func (r RemoteUser) String() string {
+	return string(r)
+}
+
+type RemoteTargetDirectory string
+
+func (r RemoteTargetDirectory) Join(name string) string {
+	return fmt.Sprintf("%s%s", r.String(), name)
+}
+
+func (r RemoteTargetDirectory) String() string {
+	return appendSlash(string(r))
+}
+
+func appendSlash(name string) string {
+	if strings.HasSuffix(name, "/") {
+		return name
+	}
+	return name + "/"
+}
+
+type PrivatePath string
+
+func (p PrivatePath) String() string {
+	return string(p)
+}
+
+func (p PrivatePath) PrivateKey() (PrivateKey, error) {
+	path, err := util.NormalizePath(p.String())
+	if err != nil {
+		return nil, fmt.Errorf("normalize path '%s' failed: %v", path, err)
+	}
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("read file '%s' failed: %v", path, err)
+	}
+	return PrivateKey(content), nil
+}
+
+type PrivateKey []byte
